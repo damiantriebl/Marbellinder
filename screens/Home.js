94685739d800206b661ea932b83137b52e90fe08
alejandra.llago <alejandra.llago@softvision.com>
2022-08-25 15:05:12 -0300

    added .env and changed the screens, now showing animation in the second screens. Created PUT route to add matches. THe players list is only showing not matched players

--- screens/Home.js
+++ screens/Home.js
@@ -1,31 +1,12 @@
-import React, { useEffect, useState, useCallback } from 'react'
-import {Card , Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
-import getMatches from '../utils/getMatches'
+import React, { useState, useCallback } from 'react'
+import { Text, View, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import { useFocusEffect } from '@react-navigation/native';
-import SwipeGesture from '../Common/swipeGesture';
-//When we use navigation.navigate() we can only navigate to screens that have been defined in the navigator
-// We can pass params: navigation.navigate('User', {id:8})
-//You can also think of the route object like a URL. Params shouldn't contain data that you think should not be in the URL
+import getMatches from '../utils/getMatches'
 
 const Home = ({ navigation }) => {
     const [matches, setMatches] = useState([])
     const [error, setError] = useState('')
-    
-    const onSwipePerformed = (action) => {     
-        switch(action){
-          case 'left':{
-            console.log('left Swipe performed');
-            break;
-          }
-           case 'right':{
-            console.log('right Swipe performed');
-            break;
-          }
-           default : {
-           console.log('Undeteceted action');
-           }
-        }
-      }
+
     const getData = async () => {
         const data = await getMatches()
         if (data.errorMessage) {
@@ -43,28 +24,23 @@ const Home = ({ navigation }) => {
 
     const renderItem = ({ item }) => {
         return (
-            <SwipeGesture  onSwipePerformed={onSwipePerformed}>
+            <View style={styles.item}>
                 <Text style={styles.name}>{item.name}</Text>
-            </SwipeGesture>
-            )
+            </View>
+        )
     }
 
-    if(!matches){
-        return <Text>Cargando</Text>
-    }
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'violet', padding: 1 }}>
-            <Text>My Matches</Text>
-                <FlatList style={{width:"100%"}}
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 50 }}>
+            <FlatList
                 data={matches}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
-                />
-            {console.log(matches[0])}
+            />
             <TouchableOpacity
                 style={styles.button}
-                onPress={() => navigation.navigate('PlayersList', { id: 8 })}
-                >
+                onPress={() => navigation.navigate('PlayersList')}
+            >
                 <Text>
                     It's a kind of Magic
                 </Text>
@@ -72,7 +48,6 @@ const Home = ({ navigation }) => {
         </View>
     )
 }
-
 
 const styles = StyleSheet.create({
     button: {
